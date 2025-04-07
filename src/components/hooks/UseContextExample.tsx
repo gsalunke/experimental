06a538d2b360464dcf04dbfd164219e9842dd168
gsalunke <ganeshharshil@gmail.com>
2025-04-07 18:03:31 +0530

    fixing import issue

--- src/components/hooks/UseContextExample.tsx
+++ src/components/hooks/UseContextExample.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useTheme } from '@/context/ThemeContext';
+import { useTheme } from '@/src/context/ThemeContext';
 
 const UseContextExample: React.FC = () => {
   const { theme, toggleTheme } = useTheme();
