c39d054210609a20db4ad300bb79baae21f19a69
gsalunke <ganeshharshil@gmail.com>
2025-04-08 09:40:34 +0530

    Removing basepath and keeping it empty

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/experimental',
+  basePath: '',
   images: {
     unoptimized: true,
   },
